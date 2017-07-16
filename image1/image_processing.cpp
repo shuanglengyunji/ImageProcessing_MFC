f46313c0806d4bf615b6e746ec8027f42e1601fb
shuanglengyunji <liuhan211211@gmail.com>
2017-07-16 23:40:02 +0800

    增加列向量适配，bool类型适配

--- image1/image_processing.cpp
+++ image1/image_processing.cpp
@@ -1,11 +1,20 @@
 #include "stdafx.h"
+#include <math.h>
 
 #include "image_processing.h"
 
+#define boolean_T bool
+
 unsigned char gray_array[IMG_WIDTH*IMG_HEIGHT] = { 0 };
+unsigned char gray_across_array[IMG_WIDTH*IMG_HEIGHT] = { 0 };
+unsigned char result_across_array[IMG_WIDTH*IMG_HEIGHT] = { 0 };
 unsigned char result_array[IMG_WIDTH*IMG_HEIGHT] = { 0 };
 float length = 0;
 float speed = 0;
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
 
 //图像处理函数
 void Image_Fix(void)	//图像算法
@@ -15,7 +24,32 @@ void Image_Fix(void)	//图像算法
 	speed = -44.2233;
 	for (int i = 0; i < IMG_WIDTH*IMG_HEIGHT; i++)
 	{
-		result_array[i] = gray_array[i] - 0x80;
+		result_across_array[i] = gray_across_array[i];
+	}
+
+}
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+void across()	//把图翻到对角的位置，生成gray_across_array
+{
+	for (int i = 0; i < 48; i++)
+	{
+		for (int j = 0; j < 80; j++)
+		{
+			gray_across_array[j * 48 + i] = gray_array[i * 80 + j];
+		}
+	}
+}
+
+void back_across()	//把图翻到对角的位置，生成gray_across_array
+{
+	for (int i = 0; i < 80; i++)
+	{
+		for (int j = 0; j < 48; j++)
+		{
+			result_array[j * 80 + i] = result_across_array[i * 48 + j];
+		}
 	}
 }
 
@@ -27,7 +61,11 @@ void processing(unsigned char * c, double * pd1, double * pd2)
 		gray_array[i] = c[i];
 	}
 
+	across();	//生成对角线图像
+
 	Image_Fix();
+
+	back_across();	//恢复正常非对角线图像
 
 	//反向拷贝
 	*pd1 = length;
